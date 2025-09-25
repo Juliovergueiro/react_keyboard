9c5dfda3e87a7d5268d6740e8e940b0354e01889
Julio Vergueiro <89145136+Juliovergueiro@users.noreply.github.com>
2025-09-25 12:04:16 -0300

    2 Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,36 @@
 /* eslint-disable react/state-in-constructor */
 import React from 'react';
-import { Clock } from './Clock';
 
 type AppState = {
-  hasClock: boolean;
-  clockName: string;
+  pressedKey: string | null;
 };
 
 export class App extends React.Component<{}, AppState> {
-  clockNameIntervalId?: number;
-
-  oldName: string = 'Clock-0';
-
   constructor(props: {}) {
     super(props);
     this.state = {
-      hasClock: true,
-      clockName: 'Clock-0',
+      pressedKey: null,
     };
   }
 
-  getRandomName = () => {
-    return `Clock-${Math.floor(Math.random() * 100)}`;
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
   };
 
   componentDidMount() {
-    // Show on left click
-    document.addEventListener('click', this.showClock);
-
-    // Hide on right click
-    document.addEventListener('contextmenu', this.hideClock);
-
-    // Update clock name every 3300ms
-    this.clockNameIntervalId = window.setInterval(() => {
-      const newName = this.getRandomName();
-
-      this.setState({ clockName: newName });
-    }, 3300);
-  }
-
-  componentDidUpdate(_prevProps: {}, prevState: AppState) {
-    if (prevState.clockName !== this.state.clockName) {
-      // eslint-disable-next-line no-console
-      console.warn(
-        `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
-      );
-    }
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('click', this.showClock);
-    document.removeEventListener('contextmenu', this.hideClock);
-
-    if (this.clockNameIntervalId) {
-      clearInterval(this.clockNameIntervalId);
-    }
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
-
-  showClock = () => {
-    this.setState({ hasClock: true });
-  };
-
-  hideClock = (event: MouseEvent) => {
-    event.preventDefault();
-    this.setState({ hasClock: false });
-  };
 
   render() {
     return (
       <div className="App">
-        {this.state.hasClock && <Clock name={this.state.clockName} />}
+        {this.state.pressedKey === null
+          ? 'Nothing was pressed yet'
+          : `The last pressed key is ${this.state.pressedKey}`}
       </div>
     );
   }
